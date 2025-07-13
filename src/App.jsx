464bc85a75fb3e6fbb4c9852c9c0b21e06a50838
Siddhant Kanawade <kanawadesiddhant30@gmail.com>
2025-07-13 17:37:17 +0530

    setting up frontend

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import LandingPage from './pages/LandingPage'
+import Navbar from './MainPages/Navbar'
+
+function App() {
+ 
+
+  return (
+    <>
+    <Navbar />
+      <LandingPage />
+    </>
+  )
+}
+
+export default App
